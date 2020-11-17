ddcce4dffa22987811a7cad6a342fa2f00a7c299
jskim <jskim@tvhub.co.kr>
2020-11-17 20:58:25 +0900

    first commit

---
+++ src/components/Chart.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="Chart">
+    <h1>chart.js test page</h1>
+  </div>
+</template>
+
+<script>
+import { LineChart } from "vue-chartjs";
+
+export default {
+  extends: LineChart,
+  name: "Chart",
+  props: {
+     chart_data: {
+      type: Array,
+      default: [],
+    },
+    options: {
+      type: Array,
+      default: [],
+    },
+  },
+  mounted() {
+    this.renderChart(this.chart_data, this.options);
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Chart chart_data="" options="" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Chart from "@/components/Chart.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Chart,
+  },
+  data() {
+    return {
+      chart_data: [],
+      options: [],
+    };
+  },
+};
 </script>
